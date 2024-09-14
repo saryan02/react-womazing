d285ce7650fd5dc19972699337ca382a1e8997e9
saryan02 <saryan.de@students.dvfu.ru>
2024-09-15 01:30:40 +1000

    added link for collection

--- src/components/home/collection/index.tsx
+++ src/components/home/collection/index.tsx
@@ -4,15 +4,17 @@ import Item from "../../collectionItem";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../redux/store";
 import {selectCloth} from "../../../redux/slices/cloth/selectors";
-
+import {Link} from "react-router-dom";
 
 
 const Collection: React.FC = () => {
     const clothes = useSelector(selectCloth)
 
     const collections = clothes.map((obj, index) => (
-        <Item key={index} name={obj.name} price={obj.price} prevPrice={obj.previously_price}
-              image={obj.image}/>
+        <Link className={styles.link} to={`/cloth/${obj.id}`}>
+            <Item key={index} name={obj.name} price={obj.price} prevPrice={obj.previously_price}
+                  image={obj.image}/>
+        </Link>
     ))
     return (<>
         <div className={`d-flex flex-column align-items-center align-items-md-start ${styles.root}`}>
